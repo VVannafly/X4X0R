3ab5eb625af5d95a3f73e4a37e64e6c5198aa827
Dmitry <wodi220500@gmail.com>
2020-09-05 22:33:34 +0300

    Initial Commit

---
+++ X4X0R/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X4X0R
+//
+//  Created by Dmitriy Chernov on 05.09.2020.
+//  Copyright © 2020 Dmitriy Chernov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
